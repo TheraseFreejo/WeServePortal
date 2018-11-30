1c335b0799cf03e9e83ccf8013a96b7eb3524837
therasefreejo@gmail.com <therasefreejo@gmail.com>
2018-12-01 00:48:28 +0400

    2 untracked files uploaded Signed-off-by: therasefreejo@gmail.com <therasefreejo@gmail.com>

---
+++ WeServe/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeServe.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public Nullable<System.DateTime> DOB { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+    }
+}
